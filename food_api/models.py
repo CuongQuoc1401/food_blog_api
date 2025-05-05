48b8a21813dca410bdf6446f419b23e4723292b2
Cuongquoc140102 <Cuongquoc140102@gmail.com>
2025-05-05 21:45:55 +0900

    first commit

---
+++ food_api/models.py
@@ -0,0 +1,38 @@
+from djongo import models
+
+class LoaiMonAn(models.Model):
+    ten_loai = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    mo_ta = models.TextField(blank=True, null=True)
+    hinh_anh_dai_dien = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.ten_loai
+
+class VungMien(models.Model):
+    ten_vung = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    mo_ta = models.TextField(blank=True, null=True)
+    ban_do = models.JSONField(blank=True, null=True) # Ví dụ: {"lat": 37.5665, "lng": 126.9780}
+    hinh_anh_dai_dien = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.ten_vung
+    
+class MonAn(models.Model):
+    ten_mon_an = models.CharField(max_length=255)
+    ten_mon_an_en = models.CharField(max_length=255, blank=True, null=True)
+    loai = models.ForeignKey(LoaiMonAn, on_delete=models.SET_NULL, null=True, related_name='mon_ans')
+    vung_mien = models.ForeignKey(VungMien, on_delete=models.SET_NULL, null=True, blank=True, related_name='mon_ans')
+    mo_ta_ngan = models.TextField(blank=True, null=True)
+    nguyen_lieu = models.JSONField()
+    cach_lam = models.JSONField()
+    hinh_anh = models.CharField(max_length=255, blank=True, null=True)
+    tags = models.JSONField(blank=True, null=True) # Ví dụ: ["thit", "cay"]
+    luot_xem = models.IntegerField(default=0)
+    ngay_tao = models.DateTimeField(auto_now_add=True)
+    ngay_cap_nhat = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.ten_mon_an
